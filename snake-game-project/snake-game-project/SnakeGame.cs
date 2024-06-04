38f928acf4112a5e4389935c00e0ed13aec947ca
Elijah Hiraka <HIRAE1@student.op.ac.nz>
2024-06-04 22:33:26 +1200

    refactoring code, added length whenever snake eats food

--- snake-game-project/snake-game-project/SnakeGame.cs
+++ snake-game-project/snake-game-project/SnakeGame.cs
@@ -15,7 +15,6 @@ namespace snake_game_project
     {
         private Random rand = new Random();
         private Point foodPosition;
-        private bool isFoodSpawned = false;
 
         // graphics
         private Bitmap offScreenBitmap;
@@ -23,16 +22,9 @@ namespace snake_game_project
         private Graphics graphics;
 
         // square variables
-        private int squareX = 0;
-        private int squareY = 0;
-        private int squareSizeX = 20;
-        private int squareSizeY = 20;
-
-        // player variable
-        private bool isDown = false;
-        private bool isUp = false;
-        private bool isLeft = false;
-        private bool isRight = false;
+        private int squareSize = 20;
+        private List<Point> snake = new List<Point>();
+        private string direction = "Right";
 
         public SnakeGame()
         {
@@ -44,7 +36,10 @@ namespace snake_game_project
             graphics = this.CreateGraphics();
             this.DoubleBuffered = true;
 
-            timer1.Interval = 200; // 16 should be approximately 60 FPS, set to 1 for more updates per second
+            snake.Add(new Point(100, 100));
+
+            timer1.Interval = 100; // 16 should be approximately 60 FPS, set to 1 for more updates per second
+            timer1.Start();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -54,136 +49,107 @@ namespace snake_game_project
         }
         private void UpdateGame()
         {
-            CheckDirection();
             MoveSnake();
-            CheckForFoodCollision();
-            SpawnFood();
+            CheckCollisions();
             DrawGrid();
+            DrawSnake();
+            SpawnFood();
         }
         private void MoveSnake()
         {
-            offScreenGraphics.FillRectangle(Brushes.Black, 0, 0, Width, Height); // changing the values 0 to 200 made it draw in a line following the snake
-            offScreenGraphics.FillRectangle(Brushes.White, squareX, squareY, squareSizeX, squareSizeY);
+            for (int i = snake.Count - 1; i > 0; i--)
+            {
+                snake[i] = snake[i - 1];
+            }
+
+            Point head = snake[0];
+            switch (direction)
+            {
+                case "Up":
+                    head.Y -= squareSize;
+                    break;
+                case "Down":
+                    head.Y += squareSize;
+                    break;
+                case "Left":
+                    head.X -= squareSize;
+                    break;
+                case "Right":
+                    head.X += squareSize;
+                    break;
+            }
+            snake[0] = head;
+
+            offScreenGraphics.Clear(Color.Black);
+        }
+        private void DrawSnake()
+        {
+            foreach (Point part in snake)
+            {
+                offScreenGraphics.FillRectangle(Brushes.White, part.X, part.Y, squareSize, squareSize);
+            }
         }
         private void SpawnFood()
         {
             if (foodPosition == Point.Empty)
             {
                 // Generate random coordinates for the food
-                int maxX = (this.Width / squareSizeX) - 1;
-                int maxY = (this.Height / squareSizeY) - 1;
-                int foodX = rand.Next(0, maxX) * squareSizeX;
-                int foodY = rand.Next(0, maxY) * squareSizeY;
+                int maxX = (this.Width / squareSize) - 1;
+                int maxY = (this.Height / squareSize) - 1;
+                int foodX = rand.Next(0, maxX) * squareSize;
+                int foodY = rand.Next(0, maxY) * squareSize;
 
                 // Set the food position
                 foodPosition = new Point(foodX, foodY);
             }
 
             // Clear previous food position
-            offScreenGraphics.FillRectangle(Brushes.Black, foodPosition.X, foodPosition.Y, squareSizeX, squareSizeY);
+            offScreenGraphics.FillRectangle(Brushes.Black, foodPosition.X, foodPosition.Y, squareSize, squareSize);
 
             // Draw the food
-            offScreenGraphics.FillRectangle(Brushes.White, foodPosition.X, foodPosition.Y, squareSizeX, squareSizeY);
+            offScreenGraphics.FillRectangle(Brushes.White, foodPosition.X, foodPosition.Y, squareSize, squareSize);
         }
 
-        private void CheckForFoodCollision()
+        private void CheckCollisions()
         {
-            // Check if the snake's head collides with the food exactly at its position
-            if (squareX == foodPosition.X && squareY == foodPosition.Y)
+            if (snake[0] == foodPosition)
             {
-                // Clear previous food position
-                offScreenGraphics.FillRectangle(Brushes.Black, foodPosition.X, foodPosition.Y, squareSizeX, squareSizeY);
-
-                // Respawn the food
+                snake.Add(new Point(-squareSize, -squareSize));  // Add new segment
                 foodPosition = Point.Empty;
                 SpawnFood();
             }
+
+            // snake runs into the walls
+            if (snake[0].X < 0 || snake[0].X >= this.Width || snake[0].Y < 0 || snake[0].Y >= this.Height)
+            {
+                // Add logic for end game
+            }
+
+            // snake runs into itself
+            for (int i = 1; i < snake.Count; i++)
+            {
+                if (snake[0] == snake[i])
+                {
+                    // add logic for end game
+                }
+            }
         }
 
-        private void CheckDirection()
-        {
-            // Update the snake's position based on direction
-            if (isUp)
-            {
-                squareY -= 20;
-                if (squareY < 0)  // If the snake hits the top border
-                {
-                    squareY = this.Height - 58;  // Move it to the bottom
-                }
-            }
-            if (isDown)
-            {
-                squareY += 20;
-                if (squareY >= this.Height - 50)  // If the snake hits the bottom border
-                {
-                    squareY = 0;  // Move it to the top
-                }
-            }
-            if (isLeft)
-            {
-                squareX -= 20;
-                if (squareX < 0)  // If the snake hits the left border
-                {
-                    squareX = this.Width - 35;  // Move it to the right
-                }
-            }
-            if (isRight)
-            {
-                squareX += 20;
-                if (squareX >= this.Width - 20)  // If the snake hits the right border
-                {
-                    squareX = 0;  // Move it to the left
-                }
-            }
-        }
-        private void MoveInput(KeyEventArgs e)
-        {
-            switch (e.KeyCode)
-            {
-                case Keys.W:
-                    isDown = false;
-                    isLeft = false;
-                    isRight = false;
-                    isUp = true;
-                    break;
-                case Keys.S:
-                    isDown = true;
-                    isLeft = false;
-                    isRight = false;
-                    isUp = false;
-                    break;
-                case Keys.D:
-                    isDown = false;
-                    isLeft = false;
-                    isRight = true;
-                    isUp = false;
-                    break;
-                case Keys.A:
-                    isDown = false;
-                    isLeft = true;
-                    isRight = false;
-                    isUp = false;
-                    break;
-            }
-        }
         private void SnakeGame_KeyDown(object sender, KeyEventArgs e)
         {
             switch (e.KeyCode)
             {
-                case Keys.Space:
-                    timer1.Start();
-                    break;
                 case Keys.W:
-                    MoveInput(e);
+                    if (direction != "Down") direction = "Up";
                     break;
                 case Keys.S:
-                    MoveInput(e);
+                    if (direction != "Up") direction = "Down";
                     break;
                 case Keys.A:
-                    MoveInput(e);
+                    if (direction != "Right") direction = "Left";
                     break;
                 case Keys.D:
-                    MoveInput(e);
+                    if (direction != "Left") direction = "Right";
                     break;
                 case Keys.Escape:
                     this.Close();
